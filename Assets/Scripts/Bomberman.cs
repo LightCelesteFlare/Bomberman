b475551e92852c37e5ad8d126d59468a0fef1773
Brandon <Brandonkool16@gmail.com>
2017-09-17 21:56:32 -0400

    [Add]

---
+++ Assets/Scripts/Bomberman.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomberman : MonoBehaviour {
+    public int Bomb;
+    public int Fire;
+    public int Speed;
+    public int maxfire, maxbomb, maxspeed;
+
+    bool facingRight = true;
+    bool facingDown = true;
+
+    private GameObject Player;
+    private GameObject Wall;
+	// Use this for initialization
+	void Start () {
+        Speed = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        GetComponent<Rigidbody2D>().velocity = targetVelocity * Speed;
+    }
+}
