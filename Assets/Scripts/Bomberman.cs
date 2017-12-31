73f637f7d91e9a8eec4f601e967faa3cf077c1eb
Brandon <Brandonkool16@gmail.com>
2017-12-31 04:32:20 -0500

    [Mod]

--- Assets/Scripts/Bomberman.cs
+++ Assets/Scripts/Bomberman.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bomberman : MonoBehaviour {
-    public int Bomb;
-    public int Fire;
-    public int Speed;
-    public int maxfire = 10, maxbomb = 9, maxspeed = 4;
+    public float Bomb;
+    public float Fire;
+    public float Speed;
+    public float maxfire = 10, maxbomb = 9, maxspeed = 4;
 
     bool facingRight = true;
     bool facingDown = true;
 
     private GameObject Player;
     private GameObject Wall;
-	// Use this for initialization
-	void Start () {
+    public GameObject Bombprefeb;
+
+    // Use this for initialization
+    void Start () {
         Speed = 1;
 	}
 	
@@ -22,5 +24,16 @@ public class Bomberman : MonoBehaviour {
 	void Update () {
         Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         GetComponent<Rigidbody2D>().velocity = targetVelocity * Speed;
+
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+            if (Input.GetKeyDown("space"))
+            {
+                Instantiate(Bombprefeb, this.gameObject.transform.position, Quaternion.identity);
+            }
+            //= new Vector3(other.transform.position.x, other.transform.position.y, 0f)
+    }
+
 }
