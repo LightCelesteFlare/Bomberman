b8c8a67c8c7300df32a66669bb1cf79e57e93ebc
Brandon <Brandonkool16@gmail.com>
2018-01-02 01:32:58 -0500

    [Mod]
    Truth is a RRRREEEEEEEEEEEEEEEeeeee D:

--- Assets/Scripts/Bomberman.cs
+++ Assets/Scripts/Bomberman.cs
@@ -3,27 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bomberman : MonoBehaviour {
-    public float Bomb;
-    public float Fire;
-    public float Speed;
+    public float Bomb = 1;
+    public float Fire = 1;
+    public float Speed = 1;
     public float maxfire = 10, maxbomb = 9, maxspeed = 4;
 
-    bool facingRight = true;
-    bool facingDown = true;
+    public int Action = 0;
+    public int Direction = 0;
 
     private GameObject Player;
     private GameObject Wall;
     public GameObject Bombprefeb;
 
+    // Animators
+    public Animator Anim;
+
     // Use this for initialization
     void Start () {
-        Speed = 1;
+        Anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         GetComponent<Rigidbody2D>().velocity = targetVelocity * Speed;
+        // Down
+        if (Input.GetKey("s") || Action == 1)
+        {
+            Anim.SetInteger("inputA", Action);
+
+        }
+        // Right
+        if (Input.GetKey("d") || Action == 2)
+        {
+            Anim.SetInteger("inputA", Action);
+        }
+        //Death
+        if (Action == 9)
+        {
+            Anim.SetInteger("inputA", Action);
+        }
+        // Idle
+        if (Action == 0)
+        {
+            Anim.SetInteger("inputA", Action);
+        }
 
     }
 
