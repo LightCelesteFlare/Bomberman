3a045c84b744928e180580dc296afcf32cc48ffd
Brandon <Brandonkool16@gmail.com>
2018-01-03 03:31:01 -0500

    [Add]

---
+++ Assets/Scripts/Snap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Snap : MonoBehaviour {
+
+    public float grid = 0.5f;
+
+    float x = 0f, y = 0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (grid > 0f)
+        {
+            float RGrid = 0.25f / grid; // reciprocalGrid
+
+            x = Mathf.Round(transform.position.x * RGrid) / RGrid;
+            y = Mathf.Round(transform.position.y * RGrid) / RGrid;
+
+            transform.position = new Vector3(x, y, transform.position.z);
+        }
+    }
+}
